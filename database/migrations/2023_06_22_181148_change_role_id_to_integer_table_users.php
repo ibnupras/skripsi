cf6bb6016a02f314a422ab9841e5430b647b0321
ibnupras <innukop@gmail.com>
2023-06-23 01:55:55 +0700

    menyambungkan model role dan user one to many

--- database/migrations/2023_06_22_181148_change_role_id_to_integer_table_users.php
+++ database/migrations/2023_06_22_181148_change_role_id_to_integer_table_users.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('role_id')->change();
+            
         });
     }
 
@@ -31,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'Admin',
+            'id' => 1,
+        ]);
+
+        Role::create([
+            'name' => 'User',
+            'id' => 2,
+        ]);
+
+    }
+}
